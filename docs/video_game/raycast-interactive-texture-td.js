bafe1f5fd7934226a218011dcf86307c62889ac1
adanfo <adanfo@uw.edu>
2022-03-14 15:16:31 -0700

    moved images

--- docs/video_game/raycast-interactive-texture-td.js
+++ docs/video_game/raycast-interactive-texture-td.js
@@ -382,9 +382,9 @@ const raycast_textures = ( sketch ) => {
     let bg;
     sketch.preload = () => {
       //img = sketch.loadImage("texture.png");
-      tex1 = sketch.loadImage("./redbrick.png");
-      tex2 = sketch.loadImage("./texture.png")
-      bg = sketch.loadImage("./image.jpg", goodLoad, badLoad);
+      tex1 = sketch.loadImage("./docs/data/images/redbrick.png");
+      tex2 = sketch.loadImage("./docs/data/images/texture.png")
+      bg = sketch.loadImage("./docs/data/images/image.jpg", goodLoad, badLoad);
     }
     //let slider2;
     sketch.setup = () => {
